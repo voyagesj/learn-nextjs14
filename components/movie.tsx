c57bd17db0dcdc4212d7b7e6126164336e691c8f
이승재/차장/네오플러스 <voyagesj@neoplus.co.kr>
2025-11-17 15:23:17 +0900

    add prefetch

--- components/movie.tsx
+++ components/movie.tsx
@@ -16,6 +16,6 @@ export default function Movie({ id, title, poster_path }: IMovieProps) {
     }
     return (<div key={id} className={styles.movie}>
         <img src={poster_path} alt={title} onClick={onClick}/>
-        <Link href={`/movies/${id}`}>{title}</Link>
+        <Link prefetch href={`/movies/${id}`}>{title}</Link>
     </div>);
 }
